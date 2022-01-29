fda83fb93cf1e13801a8bd1cc7b2c4bddcc91c8c
Dr. Montasir <montasir1400@gmail.com>
2022-01-29 21:30:52 +0300

    Creating a Simple Svelte Todo App - Svelte Firebase Todo 1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let todos = [];
+
+  let task = "";
+
+  const addTodo = () => {
+    let todo = {
+      task: task,
+      isComplete: false,
+      createdAt: new Date()
+    };
+    todos = [todo, ...todos];
+    task=""
+  };
+
+  $: console.table(todos)
+</script>
+
+<input type="text" placeholder="Add a task" bind:value={task}>
+<button on:click={addTodo}>Add</button>
+
+<ol>
+ {#each todos as item}
+    <li class:complete={item.isComplete}>{item.task}</li>
+ {/each}
+</ol>
+
+<style>
+  .complete {
+    text-decoration: line-through;
+  }
+</style>

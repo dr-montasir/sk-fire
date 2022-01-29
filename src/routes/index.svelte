68cbe8b551dd6c94e79df0d2e7612b041eac78c2
Dr. Montasir <montasir1400@gmail.com>
2022-01-30 02:16:06 +0300

    Read data from Firebase 9 - Svelte Firebase Todo 4

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,26 @@
 <script>
+	import { initializeApp, getApps, getApp } from 'firebase/app';
+	import { getFirestore, collection, onSnapshot } from 'firebase/firestore';
+	import { firebaseConfig } from '$lib/firebaseConfig';
+
+	import {browser} from '$app/env'
+
+	const firebaseApp = browser && (getApps().length === 0 ? initializeApp(firebaseConfig) : getApp());
+	
+	const db = browser && getFirestore();
+
+	const colRef = browser && collection(db, "todos")
+
 	let todos = [];
+
+	const unsubscribe = browser && onSnapshot(colRef, (querySnapshot) => {
+		let fbTodos = []
+		querySnapshot.forEach((doc) => {
+			let todo = {...doc.data(), id: doc.id}
+			fbTodos = [todo, ...fbTodos]
+		});
+		todos = fbTodos;
+	});
 
 	let task = '';
 
